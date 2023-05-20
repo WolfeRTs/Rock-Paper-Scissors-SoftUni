2f7fffa96c11d43013eabf8dcbc60205bf10c51c
Danail Tsankov <socola@abv.bg>
2023-05-20 13:59:52 +0300

    Added colored responses and restart mechanic

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -4,37 +4,47 @@ rock = 'Rock'
 paper = 'Paper'
 scissors = 'Scissors'
 
-# Player move logic
-player_input = input("Choose [R]ock, [P]aper or [S]cissors: ")
 
-if player_input.upper() == 'R':
-    player_move = rock
-elif player_input.upper() == 'P':
-    player_move = paper
-elif player_input.upper() == 'S':
-    player_move = scissors
-else:
-    raise SystemExit("Invalid Input. Try again...")
+while True:
 
-# Computer move logic
-computer_random_number = randint(1, 3)
+    # Player move logic
+    player_input = input("Choose [R]ock, [P]aper or [S]cissors: ")
 
-if computer_random_number == 1:
-    computer_move = rock
-elif computer_random_number == 2:
-    computer_move = paper
-else:
-    computer_move = scissors
+    if player_input.upper() == 'R':
+        player_move = rock
+    elif player_input.upper() == 'P':
+        player_move = paper
+    elif player_input.upper() == 'S':
+        player_move = scissors
+    else:
+        print("\033[1;31Invalid Input. Try again...")
+        continue
 
-# Player vs Computer logic and output
-print(f"You chose: {player_move}")
-print(f"Your opponent chose: {computer_move}")
+    # Computer move logic
+    computer_random_number = randint(1, 3)
 
-if (player_move == rock and computer_move == scissors) or \
-        (player_move == paper and computer_move == rock) or \
-        (player_move == scissors and computer_move == paper):
-    print("You win!")
-elif player_move == computer_move:
-    print("Draw!")
-else:
-    print("You lose!")
+    if computer_random_number == 1:
+        computer_move = rock
+    elif computer_random_number == 2:
+        computer_move = paper
+    else:
+        computer_move = scissors
+
+    # Player vs Computer logic and output
+    print(f"\033[0;36mYou chose: {player_move}")  # 36 is cyan
+    print(f"\033[0;36mYour opponent chose: {computer_move}")
+
+    if (player_move == rock and computer_move == scissors) or \
+            (player_move == paper and computer_move == rock) or \
+            (player_move == scissors and computer_move == paper):
+        print("\033[1;32mYou win!")  # 32 is green
+    elif player_move == computer_move:
+        print("\033[1;33mDraw!")  # 33 is yellow
+    else:
+        print("\033[1;31mYou lose!")  # 31 is red
+
+    # Restart or quit the game
+    end_input = input("\033[0;0mWould you like to play more?\nType [Y] to play again or [N] to quit: ")
+    if end_input.upper() == 'N':
+        print("Thank you for playing!")
+        exit()
